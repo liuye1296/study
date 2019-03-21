e7adb0921f7d9d963ee954f9c50e0625dbe6ef2f
雁北去 <478760274@qq.com>
2019-03-21 13:44:38 +0800

    数组去重复的N种方式

--- 数据结构和算法学习/数组去重.js
+++ 数据结构和算法学习/数组去重.js
@@ -52,17 +52,20 @@ const array = ['1', '2', '3', '2', '4', '5', '1', aa]
 /**
  * 排序去重 排序之后 重复的值一定会相邻 那个就可以左右对比
  */
-// function uniq3() {
-//     const array = Array.prototype.concat.apply([], Array.from(arguments))
-//     array.sort()
-//     let newArray = [array[0]]
-//     for (let i = 1, len = array.length; i < len; i++) {
-//         array[i] !== array[i - 1] && newArray.push(array[i])
-//     }
-//     return newArray
-// }
+function uniq3() {
+    const array = Array.prototype.concat.apply([], Array.from(arguments))
+    array.sort(function(a,b){
+        return a-b
+    })
+    //console.log(array.sort())
+    let newArray = [array[0]]
+    for (let i = 1, len = array.length; i < len; i++) {
+        array[i] !== array[i - 1] && newArray.push(array[i])
+    }
+    return newArray
+}
 
-// console.log(uniq3(array))
+ console.log(uniq3(array))
 
 
 /** 
@@ -78,49 +81,49 @@ const array = ['1', '2', '3', '2', '4', '5', '1', aa]
  * 利用object key 不能重复的特性 
  */
 
-function uniq5() {
-    const array = Array.prototype.concat.apply([], Array.from(arguments))
-    let obj = {}, newArray = [];
-    array.forEach((item) => {
-        if (!obj[item]) {
-            obj[item] = 1;
-            newArray.push(item)
-        }
-    });
-    obj = null;
-    return newArray
-}
-console.log(uniq5(array))
+// function uniq5() {
+//     const array = Array.prototype.concat.apply([], Array.from(arguments))
+//     let obj = {}, newArray = [];
+//     array.forEach((item) => {
+//         if (!obj[item]) {
+//             obj[item] = 1;
+//             newArray.push(item)
+//         }
+//     });
+//     obj = null;
+//     return newArray
+// }
+// console.log(uniq5(array))
 
 /**
  * includes 也可以达到效果   includes 是判断是否存在这个值
  * for of  
  */
 
-function uniq6() {
-    const array = Array.prototype.concat.apply([], Array.from(arguments))
-    let newArray = [];
-    for (let obj of array) {
-        !newArray.includes(obj) && newArray.push(obj)
-    }
-    return newArray
-}
-console.log(uniq6(array))
+// function uniq6() {
+//     const array = Array.prototype.concat.apply([], Array.from(arguments))
+//     let newArray = [];
+//     for (let obj of array) {
+//         !newArray.includes(obj) && newArray.push(obj)
+//     }
+//     return newArray
+// }
+// console.log(uniq6(array))
 
 
 /**
  * 利用Map
  */
-function uniq7() {
-    const array = Array.prototype.concat.apply([], Array.from(arguments))
-    let map = new Map, newArray = [];
-    array.forEach(item => {
-        if (!map.has(item)) {
-            map.set(item, null)
-            newArray.push(item)
-        }
-    });
-    map = null;
-    return newArray
-}
-console.log(uniq7(array))
+// function uniq7() {
+//     const array = Array.prototype.concat.apply([], Array.from(arguments))
+//     let map = new Map, newArray = [];
+//     array.forEach(item => {
+//         if (!map.has(item)) {
+//             map.set(item, null)
+//             newArray.push(item)
+//         }
+//     });
+//     map = null;
+//     return newArray
+// }
+// console.log(uniq7(array))
